ca9988b9942a978ea958c316eb703297bde41ecd
伊探究竟 <eric.chen@etonhouse.com.cn>
2024-01-04 14:19:05 +0800

    初始化界面隐藏

--- src/app/welcome/(desktop)/features/Footer.tsx
+++ src/app/welcome/(desktop)/features/Footer.tsx
@@ -16,7 +16,7 @@ const Footer = memo(() => {
   return (
     <Flexbox align={'center'} horizontal justify={'space-between'} style={{ padding: 16 }}>
       <span style={{ color: theme.colorTextDescription }}>
-        ©{new Date().getFullYear()} LobeHub
+        ©{new Date().getFullYear()} 萌鲸小秘
       </span>
       <Flexbox horizontal>
         {/* <ActionIcon
